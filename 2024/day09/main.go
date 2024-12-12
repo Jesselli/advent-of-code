364e4c23447f5321a1f62c005e3ea30eac565bc7
Justin Jesselli <jesselli@gmail.com>
2024-12-12 12:44:14 -0500

    day 9 part 2 unrevised

--- 2024/day09/main.go
+++ 2024/day09/main.go
@@ -17,50 +17,103 @@ func part1() (int, int) {
 	if err != nil {
 		panic("Unable to open input.txt")
 	}
+	input = input[:len(input)-1]
 
-	// '0' -> 48 in DEC
-	files := make([]int, 0)
+	diskSize := 0
+	for _, v := range input {
+		diskSize += int(v - 48)
+	}
+	disk := make([]int, diskSize)
+
+	fileLengths := make([]int, len(input)/2+1)
 	fileNum := 0
+	idx := 0
 	for i, v := range input {
 		itemLength := int(v - 48)
-		if v == '\n' {
-			continue
-		}
-
 		if i%2 == 0 {
 			// file
 			for j := 0; j < itemLength; j++ {
-				files = append(files, fileNum)
+				disk[idx] = fileNum
+				idx++
 			}
+			fileLengths[fileNum] = itemLength
 			fileNum++
 		} else {
 			// space -- represented by -1
 			for j := 0; j < itemLength; j++ {
-				files = append(files, -1)
+				disk[idx] = -1
+				idx++
 			}
 		}
 	}
 
-	lIdx := 0              // Points to space
-	rIdx := len(files) - 1 // Points to file
-	for lIdx != rIdx {
-		if files[lIdx] != -1 {
-			lIdx++
-			continue
-		}
-
-		if files[rIdx] == -1 {
+	rIdx := len(disk) - 1 // Points to file
+	for rIdx > 0 {
+		if disk[rIdx] == -1 {
 			rIdx--
 			continue
 		}
 
-		files[lIdx], files[rIdx] = files[rIdx], files[lIdx]
+		fileNum := disk[rIdx]
+		fileLength := fileLengths[disk[rIdx]]
+
+		lIdx := 0
+		for lIdx < rIdx {
+			if disk[lIdx] != -1 {
+				lIdx++
+				continue
+			}
+
+			spaceLength := 0
+			for i := 0; i < len(disk); i++ {
+				if lIdx+i > len(disk)-1 {
+					break
+				}
+
+				if disk[lIdx+i] != -1 {
+					break
+				} else {
+					spaceLength++
+
+				}
+			}
+
+			if fileLength <= spaceLength {
+				for i := range fileLength {
+					disk[lIdx+i] = fileNum
+					disk[rIdx-i] = -1
+				}
+				break
+			}
+			lIdx++
+		}
+
+		// Move left to the next file
+		rIdx -= fileLength
 	}
 
+	// // DEFRAG PT 1
+	// lIdx := 0              // Points to space
+	// rIdx := len(files) - 1 // Points to file
+	// for lIdx != rIdx {
+	// 	if files[lIdx] != -1 {
+	// 		lIdx++
+	// 		continue
+	// 	}
+	//
+	// 	if files[rIdx] == -1 {
+	// 		rIdx--
+	// 		continue
+	// 	}
+	//
+	// 	files[lIdx], files[rIdx] = files[rIdx], files[lIdx]
+	// }
+	// fmt.Printf("%v\n", files)
+
 	answer := 0
-	for i, v := range files {
+	for i, v := range disk {
 		if v == -1 {
-			break
+			continue
 		}
 
 		answer += i * v
